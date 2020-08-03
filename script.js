fae4c0f0c3ce3c53b622f5d29f81bb2e2c55cf39
uxlive <63720102+uxlive@users.noreply.github.com>
2020-08-03 19:45:37 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const text = document.querySelectorAll('.text');
+const halfX = window.innerWidth / 5;
+const halfY = window.innerHeight / 2;
+
+text.forEach((el, i) => {
+  TweenMax.to(el, 1, {
+    z: 1 * (i + 12) });
+
+});
+
+document.addEventListener('mousemove', e => {
+  text.forEach((el, i) => {
+    TweenMax.to(el, 0.5, {
+      x: (e.clientX - halfX) * (i + 1) * 0.01,
+      y: (e.clientY - halfY) * (i + 1) * 0.01 });
+
+  });
+});
